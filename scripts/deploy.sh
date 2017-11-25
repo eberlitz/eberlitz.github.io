7f2e67c16a32d693750d13a1061698c8f57ae71e
Eduardo Eidelwein Berlitz <eberlitz@gmail.com>
2017-11-25 19:03:51 -0200

    links page

---
+++ scripts/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+git branch -f gh-pages
+git checkout gh-pages
+echo 'Created and Checked out gh-pages branch'
+
+git reset --hard origin/master
+
+echo 'Tracking files'
+git add -A .
+echo 'Commiting files'
+git commit -a -m 'gh-pages update'
+echo 'Pushing files into gh-pages branch'
+git push origin gh-pages --force
